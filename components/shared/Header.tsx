f0382b3cdcd18182eb3415e379af17aadd2cf2dd
DevMdAsif <mdasifullah456@gmail.com>
2024-02-23 23:00:15 +0600

    header section added

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,5 +1,38 @@
+"use client";
+
+import Image from "next/image";
+import logo from "@/public/images/logo_paintters-pm62id7wvwj8wgwfwlpq1is0m2uuzuqbdznwvf6yo0.png";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const links = [
+  { label: "Home", href: "/" },
+  { label: "About", href: "/about" },
+  { label: "Contect Us", href: "/contect" },
+];
+
 function Header() {
-  return <div>Header</div>;
+  const pathname = usePathname();
+
+  return (
+    <div className="flex justify-between px-20 items-center h-20">
+      <Image src={logo} alt="" priority />
+      <ul className="flex">
+        {links.map((link, index) => (
+          <li key={index} className="mr-10">
+            <Link
+              className={`link ${
+                pathname === link.href ? "text-[#009b83]" : ""
+              } font-semibold`}
+              href={link.href}
+            >
+              {link.label}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default Header;
